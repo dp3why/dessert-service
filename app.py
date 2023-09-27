04412ec73b73389788acfc137e240119ae5bfef2
dp3why <dp3why@gmail.com>
2023-09-26 20:50:10 -0700

    creaet

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from controllers import user_controller
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Create a Flask application
+app = Flask(__name__)
+
+# routers
+app.register_blueprint(user_controller.user_blueprint)
+
+
+@app.route('/')
+def hello_world():
+    return 'server is running'
+
+
+load_dotenv()
+cors = CORS(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ services/connect.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+import os
+load_dotenv()
+
+MONGODB_CONNECTION_STRING = os.getenv("MONGO_URI")
+MONGODB_DATABASE = 'ch'
+
+# connect to mongodb
+def connect_mongo(collection_name):
+    client = MongoClient(MONGODB_CONNECTION_STRING,  tls=True,
+                         tlsAllowInvalidCertificates=True)
+    db = client[MONGODB_DATABASE]
+    collection = db[collection_name]
+
+    return client, collection
+
+
+def save_file_url_to_mongodb(filename, file_url, username):
+    client, collection = connect_mongo('upload')
+    document = {
+        'filename': filename,
+        'url': file_url,
+        'username': username
+    }
+    collection.insert_one(document)
+    client.close()
+
+
+def get_all_files_from_mongodb():
+    client, collection = connect_mongo('upload')
+
+    files = collection.find()
+
+    file_list = []
+    for file in files:
+        file_info = {
+            'filename': file['filename'],
+            'url': file['url'],
+            'username': file['username']
+        }
+        file_list.append(file_info)
+
+    client.close()
+    return file_list
